dd17d8f7e94d9e38ac739e5bf74f9c62677ee080
Rui Carmo <rui.carmo@gmail.com>
2013-03-24 16:46:26 +0000

    Added experimental Cython compile script

---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2013, Rui Carmo
+Description: Experimental Cython compile script
+License: MIT (see LICENSE.md for details)
+"""
+
+import os, sys
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+from glob import glob
+
+
+try:
+    from Cython.Distutils import build_ext
+except:
+    print "You don't seem to have Cython installed"
+    sys.exit(1)
+
+def scandir(dir, files=[]):
+    for file in os.listdir(dir):
+        path = os.path.join(dir, file)
+        if os.path.isfile(path) and path.endswith(".py"):
+            files.append(path.replace(os.path.sep, ".")[:-3])
+        elif os.path.isdir(path):
+            scandir(path, files)
+    return files
+    
+def makeExtension(extName):
+    extPath = extName.replace(".", os.path.sep)+".py"
+    return Extension(
+        extName,
+        [extPath],
+        include_dirs = ["."],
+        extra_compile_args = ["-O3", "-Wall"],
+        extra_link_args = ['-g'],
+        libraries = [],
+    )    
+
+extNames = scandir("strainer")
+#extNames.extend(scandir("bs4"))
+extNames.extend(scandir("chardet"))
+extNames.extend(scandir("html5lib"))
+extensions = [makeExtension(name) for name in extNames]
+
+setup(
+    name = "strainer",
+    packages = "strainer",
+    ext_modules=extensions,
+    cmdclass = {'build_ext': build_ext},
+)
+
